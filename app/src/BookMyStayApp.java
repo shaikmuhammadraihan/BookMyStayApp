a1d7ae42196fa220f07fdeaa4ad5f53b76bb6108
shaikmuhammadraihan <ms5216.srmist.edu>
2026-03-11 15:18:09 +0530

    dup

--- app/src/BookMyStayApp.java
+++ app/src/BookMyStayApp.java
@@ -4,6 +4,7 @@ public class BookMyStayApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Booking Management System");
         System.out.println("Application started successfully!");
+
     }
 
 }
